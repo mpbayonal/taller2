e8825adf19aab85891a1b8e9126483881b81cbc5
paolabay <mpbayonalatorre@gmail.com>
2020-08-29 07:07:14 -0500

    cambio

---
+++ Puppeteer/README.md
@@ -0,0 +1,14 @@
+# End-to-End testing With Puppeteer and Jest
+
+Starter project for tutorial
+
+### Installing
+```
+install dependencies:
+    npm install
+```
+
+### Run application
+```
+npm run dev-server
+```
---
+++ Puppeteer/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import './styles/main.scss'
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
